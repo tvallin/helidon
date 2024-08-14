8f76b486006cc2ffadab932008594936ce0ae49f
tvallin <thibault.vallin@oracle.com>
2024-08-14 10:02:02 +0200

    split the observability-output.xml

--- archetypes/archetypes/cleanup.sh
+++ archetypes/archetypes/cleanup.sh
@@ -2,8 +2,8 @@
 
 for i in $(find target/projects -type d -maxdepth 1); do
   rm -rfd $i/target
-  rm -rfd $i/target/client/target
-  rm -rfd $i/target/server/target
+  rm -rfd $i/client/target
+  rm -rfd $i/server/target
   rm -rfd $i/.idea
   rm -rfd $i/ObjectStore
   rm -rfd $i/PutObjectStoreDirHere
